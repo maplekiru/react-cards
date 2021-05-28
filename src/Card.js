92da54f1218113428609d96fe7aacd1963aeef4a
Perryvon <Perry.j.vr@gmail.com>
2021-05-28 15:20:30 -0700

    made good progress have a funcitoning but finicky card app need to fix NO click while loading and clean up some other things

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
-/**
+/** Card component 
+ * Props :
+ *  code
+ *  img 
  * 
  */
 
-function Card(){
-
+function Card({code, img, zIdx}){
+  return (
+    <div className="Card" style={{zIndex:zIdx, position:"absolute"}}  >
+      <img src={img} alt={code}></img>
+    </div>
+  )
 }
 
 export default Card
--- src/CardGame.js
+++ src/CardGame.js
@@ -4,6 +4,8 @@ import axios from 'axios'
 
 const DECK_API_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
 const API_CARD_DRAW_URL = 'https://deckofcardsapi.com/api/deck/';
+
+
 /**
  * CardGame
  * 
@@ -14,53 +16,70 @@ const API_CARD_DRAW_URL = 'https://deckofcardsapi.com/api/deck/';
  * App --> CardGame --> Card
  */
 
-
-function fetchDeckIdOnLoad() {
-  async function getDeckId() {
-    const resp = await axios.get(DECK_API_URL);
-    console.log(resp.data);
-    const id = resp.data.deck_id;
-    return id
-  }
-}
-const deckId = fetchDeckIdOnLoad()
-
 function CardGame() {
-  // const [deckId, setDeckId] = useState(null);
+  const [deckId, setDeckId] = useState(null);
   const [cardData, setCardData] = useState([]);
   const [isDrawing, setIsDrawing] = useState(false);
-
+  console.log("page set and rendered");
+  
+  
   /**
    * On 1st Render get deckId from API and set state deckId
-   */
-  // useEffect(function fetchDeckIdOnLoad(){
+   */ 
 
-  //     setDeckId(id);
-  //   }
-  //   getDeckId();
-  // },[]);
+  useEffect(function getDeckIdOnLoad() {
+    async function getDeckId() {
+      const resp = await axios.get(DECK_API_URL);
+      console.log(resp.data);
+      const id = resp.data.deck_id;
+      setDeckId(id);
+    }
+    getDeckId();
+  }, []);
 
+
+  // when isDrawing changes , pull one card from API and add 
+  // it to cardData State
   useEffect(function fetchCardOnClick() {
     async function getCard() {
       const resp = await axios.get(`${API_CARD_DRAW_URL}/${deckId}/draw`);
       console.log(resp.data);
       const card = resp.data.cards[0];
-      setCardData(cardData => [...cardData, card])
-      setIsDrawing(false);
+      setCardData(cardData => [...cardData, card]);
+      console.log("Changing is Drawing state,", isDrawing)
+      // setIsDrawing(false);
     }
-    getCard();
-  }, [isDrawing])
+    if (deckId) {
+      getCard()
+    };
+  }, [isDrawing]);
 
   /**
    * On button click draws card from API and ...
    */
   function drawCard(evt) {
-    setIsDrawing(true);
+    setIsDrawing(!isDrawing);
+  }
+  
+  
+  
+  const cardImages = cardData.map((c, idx) => <Card key={c.code} code={c.code} img={c.images.png} zIdx={idx}/>)
+  
+  // this is a filler 
+  if(cardData.length > 51){
+    return <h1>LOSER!</h1>
   }
 
   return (
     <div>
-      <button onClick={drawCard}> Draw Card </button>
+      {/*  shouldn't be able to draw card while loading */}
+      
+      <button onClick={drawCard}> Draw Card </button> :
+      
+      <div style={{display:"flex", justifyContent:"center"}}>
+      {cardImages}
+      </div>
+
     </div>
   )
 
